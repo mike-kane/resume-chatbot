1a7981a7b17d7ab30900fdfb20ddc24847c5f31a
Mike Kane <guido.vanrawesome@gmail.com>
2020-07-17 03:14:43 -0700

    complete ActionGetWeather class

--- actions.py
+++ actions.py
@@ -8,7 +8,7 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
-
+import requests as r
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
@@ -36,7 +36,22 @@ class ActionGetWeather(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
+        with open('secrets.txt', 'r') as f:
+            API_KEY = f.readline()
+
         location = tracker.get_slot('location')
-        dispatcher.utter_message(text="Getting the weather in your area.")
+        url = "https://api.openweathermap.org/data/2.5/weather?q={}&APPID={}&units=imperial".format(location, API_KEY)
+
+        response = r.get(url)
+        response_json = r.json()
+        weather = response_json['weather']
+        description = response_json['weather'][0]['description']
+        temperature = response_json['main']['temp']
+        feels_like = response_json['main']['feels_like']
+        humidity = response_json['main']['humidity']
+
+        message = """Right now in {} you can expect {}. The temperature is {} degrees (F), but it feels like {} with {}% humidity""".format(location, description, temperature, feels_like, humidity)
+
+        dispatcher.utter_message(text=message)
 
         return []
