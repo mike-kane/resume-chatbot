7879f586689c6e1ca9f5df2561ed9c88afee35e1
Mike Kane <guido.vanrawesome@gmail.com>
2020-07-19 02:12:47 -0700

    remove completed TODO

--- actions.py
+++ actions.py
@@ -23,8 +23,6 @@ class ActionSendEmail(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # TODO: complete ActionSendEmail class
-
         email = tracker.get_slot('email')
         email_resume(email)
         dispatcher.utter_message(text="Sent!")
